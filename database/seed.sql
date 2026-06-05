1b69994dd519a56f75860043e65d82e80bc46e08
dels0ne <dan.aviles@duocuc.cl>
2026-06-05 16:25:25 -0400

    fix: corregir carga inicial de servicios en seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -17,7 +17,7 @@ INSERT INTO servicio (
   id_servicio,
   nombre_servicio,
   descripcion_servicio,
-  servicio_activo
+  estado_servicio
 )
 VALUES
 (1, 'Electricidad', 'Servicios eléctricos', true),
